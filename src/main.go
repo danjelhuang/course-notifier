5ce0ef7b3636a039d474c7a5f9954e6a4e946356
Daniel Huang <daniel9huang@gmail.com>
2024-01-04 10:18:28 -0500

    run gofmt

--- src/main.go
+++ src/main.go
@@ -10,12 +10,12 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	var (
-		term string
-		year string
+		term       string
+		year       string
 		courseCode string
 	)
 
-    fmt.Println("Enter term: ")
+	fmt.Println("Enter term: ")
 	term, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error:", err)
