f2a87ee5cf69a0375f1f8425fbfd5e94d01b5248
Daniel Huang <daniel9huang@gmail.com>
2024-01-03 21:44:34 -0500

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/danjelhuang/course-notifier
+
+go 1.21.4
---
+++ src/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var (
+		term string
+		year string
+		courseCode string
+	)
+
+    fmt.Println("Enter term: ")
+	term, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	term = strings.TrimSpace(term)
+
+	fmt.Println("Enter year: ")
+	year, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	year = strings.TrimSpace(year)
+
+	fmt.Println("Enter course code: ")
+	courseCode, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	courseCode = strings.TrimSpace(courseCode)
+
+	fmt.Println(term)
+	fmt.Println(year)
+	fmt.Println(courseCode)
+}
